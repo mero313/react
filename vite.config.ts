ec632042dac77f46dde5f92380926c645b6834d1
root <root@MeRo.localdomain>
2025-09-30 16:30:27 +0300

    edits some things1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react/", // YOUR REPO NAME HERE
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  
 });
