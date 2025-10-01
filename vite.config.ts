c8fce037083047f560178e0c68bdef1b2ca4c829
root <root@MeRo.localdomain>
2025-10-01 12:44:12 +0300

    edint repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react/", // YOUR REPO NAME HERE
+  base: "/MeRo_Dev/", // YOUR REPO NAME HERE
   plugins: [react()],
   
 });
